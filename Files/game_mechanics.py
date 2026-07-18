38ba731867cc926ed765e2ba834aef44bcf68dc5
Fernandez Mathys <mathys.fernandez5572@gmail.com>
2026-07-18 09:38:31 +0200

    fix: modification of comments in game_mechanics

--- Files/game_mechanics.py
+++ Files/game_mechanics.py
@@ -5,11 +5,13 @@ except ImportError:
     import settings
 
 def gerer_dash(events, temps_actuel, deplacement_x, deplacement_y, vitesse):
+
     # Vérifier si on peut lancer le dash
     # Que si le joueur est entrain de bouger ou que le cooldown du dash n'est pas fini
     if not settings.dash_actif and temps_actuel >= settings.dash_cooldown_fin:
         for event in events:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                
                 # On vérifie qu'il y a un mouvement (pour ne pas dasher sur place)
                 if deplacement_x != 0 or deplacement_y != 0:
                     settings.dash_actif = True
@@ -26,12 +28,16 @@ def gerer_dash(events, temps_actuel, deplacement_x, deplacement_y, vitesse):
                     elif deplacement_y < 0: dir_y = -1
                     
                     settings.dash_dir = (dir_x, dir_y)
-                break # Une seule action par frame
+
+                # Une seule action par frame
+                break 
 
     # Appliquer l'effet du dash si actif
     if settings.dash_actif:
         if temps_actuel >= settings.dash_fin_temps:
-            settings.dash_actif = False # Fin du dash
+
+            # Fin du dash
+            settings.dash_actif = False 
         else:
             deplacement_x = settings.dash_dir[0]
             deplacement_y = settings.dash_dir[1]
